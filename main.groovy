796014dfbd357df0e3116539c854583cb659dd75
harish628 <90142836+harish628@users.noreply.github.com>
2023-05-01 16:40:01 +0530

    testing hello

--- main.groovy
+++ main.groovy
@@ -1,34 +1,49 @@
+// pipeline {
+//     agent any
+    
+//     stages {
+//         stage('Build') {
+//             steps {
+//                 // Build your project here
+//             }
+//         }
+//     }
+    
+//     post {
+//         always {
+//             // Define the email recipient(s)
+//             def to = 'harish.gundimeda@gmail.com'
+            
+//             // Define the email subject
+//             def subject = "Jenkins build ${currentBuild.fullDisplayName} has completed"
+            
+//             // Define the email body
+//             def body = "Build details:\n\n" +
+//                        "Status: ${currentBuild.result}\n" +
+//                        "Duration: ${currentBuild.durationString}\n" +
+//                        "Build log: ${env.BUILD_URL}console\n\n" +
+//                        "Thanks,\n" +
+//                        "Jenkins"
+            
+//             // Send the email using the Jenkins Mailer plugin
+//             mail to: to,
+//                  subject: subject,
+//                  body: body
+//         }
+//     }
+// }
 pipeline {
     agent any
-    
+
+    parameters {
+        string(name: 'GREETING', defaultValue: 'Hello', description: 'Greeting message')
+    }
+
     stages {
         stage('Build') {
             steps {
-                // Build your project here
+                echo "${params.GREETING}, world!"
             }
-        }
-    }
-    
-    post {
-        always {
-            // Define the email recipient(s)
-            def to = 'harish.gundimeda@gmail.com'
-            
-            // Define the email subject
-            def subject = "Jenkins build ${currentBuild.fullDisplayName} has completed"
-            
-            // Define the email body
-            def body = "Build details:\n\n" +
-                       "Status: ${currentBuild.result}\n" +
-                       "Duration: ${currentBuild.durationString}\n" +
-                       "Build log: ${env.BUILD_URL}console\n\n" +
-                       "Thanks,\n" +
-                       "Jenkins"
-            
-            // Send the email using the Jenkins Mailer plugin
-            mail to: to,
-                 subject: subject,
-                 body: body
         }
     }
 }
